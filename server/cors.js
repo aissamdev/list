49ca9c06d24a75728c9d27a2a61f6b345ce56532
Aissam <aissamaot@gmail.com>
2024-06-26 16:06:01 +0200

    first commit

--- server/cors.js
+++ server/cors.js
@@ -6,14 +6,6 @@ const ACCEPTED_ORIGINS = [
 
 export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => cors({
   origin: (origin, callback) => {
-    if (acceptedOrigins.includes(origin)) {
-      return callback(null, true)
-    }
-
-    if (!origin) {
-      return callback(null, true)
-    }
-
-    return callback(new Error('Not allowed by CORS'))
+    return callback(null, true)
   }
 })
