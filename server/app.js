6aa88b164f641fabdfad35552f1374f2cccc8b3b
Aissam <aissamaot@gmail.com>
2024-06-26 15:59:40 +0200

    first commit

--- server/app.js
+++ server/app.js
@@ -11,13 +11,15 @@ app.use(corsMiddleware())
 
 app.disable('x-powered-by')
 
+
+
 app.post('/', (req, res) => {
   list.push(req.body)
   console.log(list)
   res.json(req.body)
 })
 
-const PORT = process.env.PORT ?? 1234
+const PORT = process.env.PORT ?? 10000
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
--- web/script.js
+++ web/script.js
@@ -1,14 +1,15 @@
 function create() {
-  console.log("Button pressed")
+  console.log("Button pressedd")
   let input = document.querySelector('#input')
   const data = {
     value: input.value
   }
-  fetch('http://localhost:1234', {
+  fetch('https://dynamic-list.onrender.com', {
     method: 'POST',
+    mode: 'no-cors',
     headers: {
       "Content-Type": "application/json"
     },
     body: JSON.stringify(data)
-  })
+  }).then(res => res.json()).then(data => console.log(data))
 }
